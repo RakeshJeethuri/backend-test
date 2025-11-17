a493268aba75daf23f2d58d221d5c247b32ce535
RakeshJeethuri <rakeshjeethuri@gmail.com>
2025-11-17 11:56:55 +0530

    updated files

--- src/app/api/tests/route.ts
+++ src/app/api/tests/route.ts
@@ -2,5 +2,8 @@ import { NextResponse } from "next/server";
 
 //write a test api
 export async function GET() {
-    return NextResponse.json({ message: "Hello, world!" });
+    return NextResponse.json({
+    version: "1.1.1",
+    isMaintenance: true,
+    isAllowedDevice: true,});
 }
