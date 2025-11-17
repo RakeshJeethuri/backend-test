9c2057b82ee4798dc6b3907866dee9eb333d2624
RakeshJeethuri <rakeshjeethuri@gmail.com>
2025-11-17 12:43:29 +0530

    updated files

--- src/app/api/tests/route.ts
+++ src/app/api/tests/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 //write a test api
 export async function GET() {
     return NextResponse.json({
-        isUnderMaintenance: true,
+        isUnderMaintenance: false,
         currentVersion: "1.4.0",
-        minimumRequiredVersion: "1.10",
+        minimumRequiredVersion: "1.1.0",
         maintenanceMessage: "We are currently performing scheduled maintenance to improve your experience.",
         maintenanceEndTime: new Date(Date.now() + 60 * 60 * 1000).toISOString()
     });
